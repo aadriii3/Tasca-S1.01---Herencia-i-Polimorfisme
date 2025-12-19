009560de1504a1c920921763833179a72d8c2d34
Adri Quiñoa <adriaquinoajimenez@gmail.com>
2025-12-19 15:20:07 +0100

    fix: removed unused methods and deleted comments from exercise 1 level 2

--- level2/src/main/java/org/example/exercici1/model/Phone.java
+++ level2/src/main/java/org/example/exercici1/model/Phone.java
@@ -5,22 +5,11 @@ public class Phone{
     private final String model;
     private final String brand;
 
-    /*Constructor*/
     public Phone(String brand, String model) {
         this.brand = brand;
         this.model = model;
     }
 
-    /*Getters and Setters*/
-    public String getModel() {
-        return model;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    /*Methods*/
     public void doCalls(int number){
         System.out.println("S'està trucant al número: " + number);
     }
